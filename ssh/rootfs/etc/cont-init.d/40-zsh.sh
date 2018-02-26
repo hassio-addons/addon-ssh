e6cef2c641130314667a8a221b561fe741db509a
Franck Nijhof <frenck@addons.community>
2018-02-26 22:30:51 +0100

    :ambulance: Adds HASSIO_TOKEN to user environment
    Signed-off-by: Franck Nijhof <frenck@addons.community>

--- ssh/rootfs/etc/cont-init.d/40-zsh.sh
+++ ssh/rootfs/etc/cont-init.d/40-zsh.sh
@@ -8,12 +8,16 @@ source /usr/lib/hassio-addons/base.sh
 
 readonly ZSH_HISTORY_FILE=/root/.zsh_history
 readonly ZSH_HISTORY_PERSISTANT_FILE=/data/.zsh_history
+readonly ZSH_ENVIRONMENT_FILE=/root/.zshenv
 
 touch "${ZSH_HISTORY_PERSISTANT_FILE}" \
     || hass.die 'Failed creating a persistent ZSH history file'
-    
+
 chmod 600 "$ZSH_HISTORY_PERSISTANT_FILE" \
     || hass.die 'Failed setting the correct permissions to the ZSH history file'
 
 ln -s -f "$ZSH_HISTORY_PERSISTANT_FILE" "$ZSH_HISTORY_FILE" \
     || hass.die 'Failed linking the persistant ZSH history file'
+
+echo "export HASSIO_TOKEN=\"${HASSIO_TOKEN}\"" > "${ZSH_ENVIRONMENT_FILE}" \
+    || hass.die 'Failed to export Hassio API token'
