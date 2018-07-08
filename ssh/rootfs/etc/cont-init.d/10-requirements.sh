794809b572fa8f6947ab46f6cd545903586284ed
Franck Nijhof <frenck@addons.community>
2018-07-08 22:05:36 +0200

    :hammer: Initial addition of ttyd

--- ssh/rootfs/etc/cont-init.d/10-requirements.sh
+++ ssh/rootfs/etc/cont-init.d/10-requirements.sh
@@ -35,3 +35,31 @@ if hass.config.has_value 'password'; then
     hass.log.warning 'Logging in with a password is security wise, a bad idea!'
     hass.log.warning 'Please, consider using a public/private key pair'
 fi
+
+# Check if username password as set together
+if hass.config.has_value 'username' && ! hass.config.has_value 'password'; then
+    hass.die 'You have set an username, but no password!'
+fi
+
+if ! hass.config.has_value 'username' && hass.config.has_value 'password'; then
+    hass.die 'You have set a password, but no username!'
+fi
+
+# Check SSL requirements, if enabled
+if hass.config.true 'ssl'; then
+    if ! hass.config.has_value 'certfile'; then
+        hass.die 'SSL is enabled, but no certfile was specified'
+    fi
+
+    if ! hass.config.has_value 'keyfile'; then
+        hass.die 'SSL is enabled, but no keyfile was specified'
+    fi
+
+    if ! hass.file_exists "/ssl/$(hass.config.get 'certfile')"; then
+        hass.die 'The configured certfile is not found'
+    fi
+
+    if ! hass.file_exists "/ssl/$(hass.config.get 'keyfile')"; then
+        hass.die 'The configured keyfile is not found'
+    fi
+fi
