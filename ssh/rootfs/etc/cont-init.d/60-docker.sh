bba379b48464de842838b566b9ec9ba5e8115c66
Franck Nijhof <frenck@addons.community>
2019-03-17 21:12:34 +0100

    :ambulance: Fixes Docker commands

--- ssh/rootfs/etc/cont-init.d/60-docker.sh
+++ ssh/rootfs/etc/cont-init.d/60-docker.sh
@@ -4,7 +4,7 @@
 # Sets up shared sessions between the SSH & Web terminal
 # ==============================================================================
 # Disable SSH & Web Terminal session sharing if configured
-if ! bashio::addon.protected; then
+if bashio::var.false "$(bashio::addon.protected)"; then
     rm /usr/local/bin/docker
     mv /usr/local/bin/.undocked /usr/local/bin/docker
     bashio::log.info 'Docker support has been enabled.'
