d121692f167a40cdfcb06c8d4d5ddf38cc670323
Franck Nijhof <git@frenck.dev>
2022-06-07 17:28:55 +0200

    ⬆️ Upgrades add-on base image to 12.0.0 (#410)
    * ⬆️ Upgrades add-on base image to 12.0.0
    
    * Adjust wheels location
    
    * Use container env for correct logging

--- ssh/rootfs/etc/cont-init.d/mysql.sh
+++ ssh/rootfs/etc/cont-init.d/mysql.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bashio
+#!/command/with-contenv bashio
 # ==============================================================================
 # Home Assistant Community Add-on: SSH & Web Terminal
 # Pre-configures the MySQL clients, if the service is available
--- ssh/rootfs/etc/cont-init.d/user.sh
+++ ssh/rootfs/etc/cont-init.d/user.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bashio
+#!/command/with-contenv bashio
 # ==============================================================================
 # Home Assistant Community Add-on: SSH & Web Terminal
 # Executes configured customizations & persists user settings
