7d25eefeb86d2a65f1a1ebddc81da594db51edeb
Ville Skyttä <ville.skytta@iki.fi>
2022-11-20 09:27:27 +0200

    fix(bash): use hash mark in root prompt
    `#` for root, `$` for non-root is a long standing tradition in
    bash. Presenting the dollar for root may lead to dangerous incorrect
    assumptions.

--- ssh/rootfs/etc/profile.d/hassio.sh
+++ ssh/rootfs/etc/profile.d/hassio.sh
@@ -1,4 +1,9 @@
-export PS1='\W $ '
+export PS1='\W'
+if [ "${USER-}" = root ]; then
+    PS1="$PS1 # "
+else
+    PS1="$PS1 \$ "
+fi
 case "${TERM-}" in
     rxvt*|vte*|xterm*) PS1='\[\e]0;\u@\h:\w\a\]'"$PS1" ;;
 esac
